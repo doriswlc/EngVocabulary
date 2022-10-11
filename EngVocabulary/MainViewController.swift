13e2cd8540f0b04595c1122766b3201031a17836
Doris Chang <doriswlc@gmail.com>
2022-10-11 13:44:58 +0800

    add index for each alphabet

--- EngVocabulary/MainViewController.swift
+++ EngVocabulary/MainViewController.swift
@@ -9,33 +9,50 @@ import UIKit
 
 class MainViewController: UIViewController {
     var words = [Vocabulary]()
+    var alphabet = "A"
+    var index = 0
     @IBOutlet var alphabetButton: [UIButton]!
     @IBOutlet weak var enLabel: UILabel!
     @IBOutlet weak var enSentenseLabel: UILabel!
     @IBOutlet weak var chLabel: UILabel!
     @IBOutlet weak var chSentenseLabel: UILabel!
-    
+    @IBOutlet weak var remarkLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
-
+    func updateUI() {
+        enLabel.text = words[index].en
+        enSentenseLabel.text = words[index].enSentence
+        chLabel.text = words[index].ch
+        chSentenseLabel.text = words[index].chSentence
+        remarkLabel.text = "字母\(alphabet)的第\(index + 1)筆資料"
+    }
+    
     @IBAction func alphabetButtonTap(_ sender: UIButton) {
         words = []
-        let alphabet: String = (alphabetButton[alphabetButton.firstIndex(of: sender)!].titleLabel?.text)!
-        if let data = NSDataAsset(name: String(describing: alphabet))?.data, let content = String(data: data, encoding: .utf16) {
+        alphabet = (alphabetButton[alphabetButton.firstIndex(of: sender)!].titleLabel?.text)!
+        if let data = NSDataAsset(name: alphabet)?.data, let content = String(data: data, encoding: .utf16) {
             let array = content.components(separatedBy: "\n")
             for line in array {
                 let elements = line.components(separatedBy: "\t")
                 words.append(Vocabulary(en: elements[0], ch: elements[1], enSentence: elements[2], chSentence: elements[3]))
             }
-            enLabel.text = words[0].en
-            enSentenseLabel.text = words[0].enSentence
-            chLabel.text = words[0].ch
-            chSentenseLabel.text = words[0].chSentence
+            updateUI()
         }
     }
     
+    @IBAction func Next(_ sender: UIButton) {
+        index = (index + 1) % words.count
+        updateUI()
+    }
+    @IBAction func Pre(_ sender: UIButton) {
+        index -= 1
+        if index < 0 {
+            index = words.count - 1
+        }
+        updateUI()
+    }
 }
 
